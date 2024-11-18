77f8ca1e349327226637d1243078d1561e1ae79c
davuad <davuandrias01@gmail.com>
2024-11-18 15:46:59 +0700

    Menghapus

--- app/controllers/ProgramStudiController.php
+++
@@ -1,9 +0,0 @@
-<?php
-class ProgramStudiController {
-  public function index() {
-    require_once "models/ProgramStudi_model.php";
-    $ins = new ProgramStudi_model;
-    $data = $ins->programStudiView();
-    require_once "views/program_studi_view.php";
-  }
-}
